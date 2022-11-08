1d527b91a60cbbeab143a8e992e68b9d24713849
Tim Quattrochi <timq82@gmail.com>
2022-11-07 23:06:29 -0500

    imported footer and skills components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Landing from '../src/components/Landing';
 import About from './components/About';
 import Card from './components/Card';
+import Footer from './components/Footer';
+import Skills from './components/Skills';
 const App = () => {
   return (
     <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
       <Card />
       <About />
+      <Skills />
+      <Footer />
     </div>
   );
 };
