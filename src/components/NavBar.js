784346d360359b12d331e81e874fa084a6639f70
Tim Quattrochi <timq82@gmail.com>
2022-11-12 12:16:33 -0500

    added fire gif icon to nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,102 @@
-import React from 'react';
+import { useState } from 'react';
+import icon from '../assets/icons8-fire.gif';
 
 function NavBar() {
-  return <div>NavBar</div>;
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  return (
+    <>
+      <div
+        id="navigation"
+        className="flex items-center justify-between lg:justify-around border-b border-gray py-8 pb-0"
+      >
+        <a href="/">
+          <img
+            className="object-contain h-20 w-50"
+            src={icon}
+            alt="logo"
+          />
+        </a>
+        <nav>
+          <section className="mobile-menu flex lg:hidden">
+            <div
+              className="hamburger-icon px-8 space-y-2"
+              onClick={() => setIsNavOpen((prev) => !prev)}
+            >
+              <span className="block h-0.5 w-8 animate-pulse bg-blue"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-blue"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-blue"></span>
+            </div>
+
+            <div
+              className={isNavOpen ? 'showMenuNav' : 'hideMenuNav'}
+            >
+              {' '}
+              <div
+                className="x-icon absolute top-0 right-0 px-8 py-8"
+                onClick={() => setIsNavOpen(false)}
+              >
+                <svg
+                  className="h-8 w-8 text-gray"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <line x1="18" y1="6" x2="6" y2="18" />
+                  <line x1="6" y1="6" x2="18" y2="18" />
+                </svg>
+              </div>
+              <ul className="menu-link-mobile-open flex flex-col items-center justify-between min-h-[250px]">
+                <li className="border-b border-gray-400 my-8 uppercase">
+                  <a href="/about">About</a>
+                </li>
+                <li className="border-b border-gray-400 my-8 uppercase">
+                  <a href="/portfolio">Portfolio</a>
+                </li>
+                <li className="border-b border-gray-400 my-8 uppercase">
+                  <a href="/contact">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </section>
+
+          <ul className="desktop-menu-hidden hidden space-x-8 lg:flex">
+            <li>
+              <a href="/about">Projects</a>
+            </li>
+            <li>
+              <a href="/about">Resume</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
+      </div>
+    </>
+  );
 }
 
 export default NavBar;
