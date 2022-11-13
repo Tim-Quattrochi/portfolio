ae41afd1cced7ed6cdc4614a34c62fb8b1040256
Tim Quattrochi <timq82@gmail.com>
2022-11-12 23:52:48 -0500

    Refactored to use routes through app.js and langing as '/'

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import { useEffect } from 'react';
-import About from './components/About';
-import Card from './components/Card';
-import Footer from './components/Footer';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
+import { Routes, Route, Link } from 'react-router-dom';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
+import Landing from './pages/Landing';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
 const App = () => {
-  useEffect(() => {
-    Aos.init({
-      once: true,
-    });
-  }, []);
-
   return (
-    <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
-      <div data-aos="fade-down" data-aos-duration="800">
-        <Card />
-      </div>
-      <div
-        data-aos="fade-up"
-        data-aos-duration="800"
-        data-aos-delay="400"
-      >
-        <About />
-      </div>
-      <Skills />
-      <Contact />
-      <Footer />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="resume" element={<Resume />} />
+        <Route path="projects" element={<Projects />} />
+
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <h1>
+                404: Page Error. Page not found. Please navigate back
+                using one of the links above.
+              </h1>
+            </main>
+          }
+        />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/pages/Landing.js
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+
+import About from '../components/About';
+import Card from '../components/Card';
+import Footer from '../components/Footer';
+import Skills from '../components/Skills';
+import Contact from '../components/Contact';
+import NavBar from '../components/NavBar';
+import Resume from '../pages/Resume';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
+const Landing = () => {
+  useEffect(() => {
+    Aos.init({
+      once: true,
+    });
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
+        <div data-aos="fade-down" data-aos-duration="800">
+          <Card />
+        </div>
+        <div
+          data-aos="fade-up"
+          data-aos-duration="800"
+          data-aos-delay="400"
+        >
+          <About />
+        </div>
+
+        <Skills />
+        <Contact />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Landing;
