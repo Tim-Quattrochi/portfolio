b9f3bc8e1730dda7e475988f99e1ba606cfa32dd
Tim Quattrochi <timq82@gmail.com>
2022-11-15 01:00:11 -0500

    testing mobile pdf view

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -7,16 +7,25 @@ import Footer from '../components/Footer';
 import { Document, Page, pdfjs } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+const options = {
+  cMapUrl: 'cmaps/',
+  cMapPacked: true,
+  standardFontDataUrl: 'standard_fonts/',
+};
+
 function resume() {
   return (
     <>
       <NavBar />
-      <Document
-        className="flex justify-center items-center h-screen sm:object-contain"
-        file={draftResume}
-      >
-        <Page scale={1.5} width={350} pageNumber={1} />
-      </Document>
+      <div className="sm:min-w-full max-w-xl flex flex-col align-center mx-auto ">
+        <Document
+          className="mx-auto"
+          file={draftResume}
+          options={options}
+        >
+          <Page size="A4" height={600} pageIndex={0} />
+        </Document>
+      </div>
       <Footer />
     </>
   );
