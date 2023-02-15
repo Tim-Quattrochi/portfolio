c6d1a437f2277557f3667cfb66d3ceb3a761ce81
Tim Quattrochi <timq82@gmail.com>
2023-02-15 18:59:03 -0500

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
-import Projects from './pages/Projects';
-import Resume from './pages/Resume';
-import Landing from './pages/Landing';
-import 'aos/dist/aos.css';
-import ContactForm from './pages/ContactForm';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import ReactGA from "react-ga";
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+import Landing from "./pages/Landing";
+import "aos/dist/aos.css";
+import ContactForm from "./pages/ContactForm";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const TRACKING_ID = "UA-257368782-1";
+ReactGA.initialize(TRACKING_ID);
 
 const App = () => {
+  useEffect(() => {
+    ReactGA.pageview(
+      window.location.pathname + window.location.search
+    );
+  }, []);
   return (
     <>
       <Routes>
@@ -20,7 +30,7 @@ const App = () => {
         <Route
           path="*"
           element={
-            <main style={{ padding: '1rem' }}>
+            <main style={{ padding: "1rem" }}>
               <h1>
                 404: Page Error. Page not found. Please navigate back
                 using one of the links above.
