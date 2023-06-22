f24b301fbc541f2caec6231d371f9b0e5d9a38dc
Tim Quattrochi <timq82@gmail.com>
2023-06-21 22:37:48 -0400

    converted images to webp

---
+++ src/assets/webp/index.js
@@ -0,0 +1,8 @@
+export { default as studyBoard } from "./studyBoard.webp";
+export { default as tpd2 } from "./tpd2.webp";
+export { default as guestBook } from "./guestBook.webp";
+export { default as upfront } from "./upfront.webp";
+export { default as nt } from "./n-t.webp";
+export { default as headshot } from "./headShot.webp";
+export { default as devTeam } from "./devTeam.webp";
+export { default as rectangle42 } from "./studyBoard.webp";
