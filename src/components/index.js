0227f9c6d6c0ba231a704f6009761cbcd5fce598
Tim Quattrochi <timq82@gmail.com>
2023-09-22 18:38:36 -0400

    Barrel Export file for components

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { default as Footer } from "./Footer";
+export { default as NavBar } from "./NavBar/NavBar";
+export { default as MobileNav } from "./NavBar/MobileNav";
+export { default as Card } from "./Card";
+export { default as About } from "./About";
+export { default as Skills } from "./Skills/Skills";
+export { default as ProjectCard } from "./ProjectCard/ProjectCard";
+export { default as Divider } from "./Divider";
+export { default as DarkmodeToggle } from "./DarkMode/DarkmodeToggle";
+export { default as LoadingSpinner } from "./LoadingSpinner";
+export { default as NotFound } from "./NotFound";
