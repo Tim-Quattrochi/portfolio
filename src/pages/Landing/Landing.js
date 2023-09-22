003290de2174b8b1926091a8ffa49032b7fe4b8c
Tim Quattrochi <timq82@gmail.com>
2023-09-22 19:38:43 -0400

    imported components from barrel export file

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
-import About from "../../components/About";
-import Card from "../../components/Card";
-import Footer from "../../components/Footer";
-import Skills from "../../components/Skills/Skills";
-import NavBar from "../../components/NavBar/NavBar";
+import {
+  Footer,
+  NavBar,
+  About,
+  Card,
+  Divider,
+  Skills,
+} from "../../components";
 import { projects } from "../../utils/projects";
 import Aos from "aos";
-import Divider from "../../components/Divider";
 import "aos/dist/aos.css";
 import Projects from "../Projects";
 
@@ -20,8 +22,9 @@ const Landing = () => {
   return (
     <>
       <NavBar />
+
       <div className="font-neueMachina min-h-screen pb-0 py-10 px-3 sm:px-5 bg-[#F5F5F5]">
-        <div className="w-half bg-primary py-10 rounded-lg">
+        <div className="w-half bg-primary dark:bg-text-gray-dark py-10 rounded-lg">
           <div data-aos="fade-down" data-aos-duration="800">
             <Card />
           </div>
