d01f2fb74480aaabe6244d46d1a4069286b5be2c
Tim Quattrochi <timq82@gmail.com>
2023-09-22 19:33:08 -0400

    added darkMode class to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
     colors: {
       "back-ground": "#F7F7F7",
