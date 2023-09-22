6c5d53adf820e4c248b7c6c0f713a1601f611261
Tim Quattrochi <timq82@gmail.com>
2023-09-22 19:34:08 -0400

    updated export because of file name change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { DarkModeProvider } from "./context/DarkModeProvider";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <DarkModeProvider>
+      <App />
+    </DarkModeProvider>
   </BrowserRouter>
 );
