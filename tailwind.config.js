7b4d9261779478f8785ffc49526020a7c0b57cbf
Tim Quattrochi <timq82@gmail.com>
2023-09-22 20:39:41 -0400

    moved colors to extend so I can use colors not in theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,19 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
-    colors: {
-      "back-ground": "#F7F7F7",
-      primary: "#2274A5",
-      secondary: "#32936F",
-      accent: "#EF6C35",
-      text: "#333333",
-      white: "#FFFFFF",
-      twitter: "#1DA1F2",
-      linkedIn: "#0077B5",
-      "gray-light": "#d3dce6",
-      "text-gray-dark": "#273444",
-    },
     extend: {
+      colors: {
+        "back-ground": "#F7F7F7",
+        primary: "#2274A5",
+        secondary: "#32936F",
+        accent: "#EF6C35",
+        text: "#333333",
+        white: "#FFFFFF",
+        twitter: "#1DA1F2",
+        linkedIn: "#0077B5",
+        "gray-light": "#d3dce6",
+        "text-gray-dark": "#273444",
+      },
       fontFamily: {
         neueMachina: ["Neue Machina", "sans-serif"],
       },
