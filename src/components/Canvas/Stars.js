34571c1764e02471cd4c85c7011dd0dd57b69990
Tim Quattrochi <timq82@gmail.com>
2023-09-23 11:28:34 -0400

    created floating stars

---
+++ src/components/Canvas/Stars.js
@@ -0,0 +1,52 @@
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.esm";
+
+const Stars = (props) => {
+  const ref = useRef();
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5001), { radius: 1.5 })
+  );
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
+        ref={ref}
+        positions={sphere}
+        stride={3}
+        frustumCulled
+        {...props}
+      >
+        <PointMaterial
+          transparent
+          color="#afc9ff"
+          size={0.004}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
+
+const StarsCanvas = () => {
+  return (
+    <div className="absolute inset-x-0 bottom-0 w-full h-96 z-[2]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
