54a233905329b0eb56d2058214da18bd8f54946b
Tim Quattrochi <timq82@gmail.com>
2023-09-23 12:22:06 -0400

    dark mode options, added modal

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Modal from "../Modal/MyModal";
 
 const ProjectCard = ({
   name,
@@ -7,63 +8,57 @@ const ProjectCard = ({
   tech,
   link,
   link2,
+  isModalOpen,
+  openModal,
+  closeModal,
+  currentIndex,
+  nextProject,
+  previousProject,
+  projects,
 }) => {
-  const [hovered, setHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    setHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setHovered(false);
-  };
-
   return (
     <div
-      className="relative rounded-lg overflow-hidden h-full"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
+      className="relative rounded-lg overflow-hidden h-full group"
+      onClick={(e) => openModal(e, currentIndex)}
     >
       <img
         src={image}
         alt="work"
-        className="w-full h-36 md:h-48 object-cover"
+        className="w-full h-48 md:h-64 object-cover transition-transform transform group-hover:scale-105"
       />
-      {hovered && (
-        <div className="absolute inset-0 bg-back-ground text-text flex flex-col justify-center items-center p-5 bg-opacity-90 transition-opacity duration-300">
-          <h3 className="text-md md:text-xl mb-1 md:mb-3 font-semibold text-center">
-            {name}
-          </h3>
-          <p className="text-xs mb-2 text-center font-bold text-secondary leading-none">
-            {description}
-          </p>
+      <div className="absolute inset-0 bg-gradient-to-t from-primary to-secondary text-white opacity-0 transition-opacity duration-300 group-hover:opacity-90 flex flex-col justify-center items-center p-5  cursor-pointer">
+        <h3 className="text-md md:text-xl mb-1 md:mb-3 font-semibold text-center">
+          {name}
+        </h3>
+        <p className="text-xs mb-2 text-center font-bold text-white leading-none bg-opacity-90 bg-dark-accent-1 rounded-md">
+          {description}
+        </p>
 
-          <div className="flex justify-center space-x-4 mt-2">
-            {link && (
-              <a
-                href={link}
-                target="_blank"
-                rel="noreferrer"
-                className="text-text bg-accent hover:text-blue transition-colors duration-300 px-3 py-2 rounded-md"
-              >
-                Live Demo
-              </a>
-            )}
-            {link2 && (
-              <a
-                href={link2}
-                target="_blank"
-                rel="noreferrer"
-                className="text-[#000080] bg-primary hover:text-secondary transition-colors duration-300 px-3 py-2 rounded-md"
-              >
-                GitHub
-              </a>
-            )}
-          </div>
+        <div className="flex justify-center space-x-4 mt-2">
+          {link && (
+            <a
+              href={link}
+              target="_blank"
+              rel="noreferrer"
+              className="text-text bg-accent hover:text-blue transition-colors duration-300 px-3 py-2 rounded-md"
+            >
+              Live Demo
+            </a>
+          )}
+          {link2 && (
+            <a
+              href={link2}
+              target="_blank"
+              rel="noreferrer"
+              className="text-[#000080] bg-primary hover:text-secondary transition-colors duration-300 px-3 py-2 rounded-md"
+            >
+              GitHub
+            </a>
+          )}
         </div>
-      )}
+      </div>
       <div className="bg-primary p-5 text-text-gray-dark">
-        <h3 className="text-sm md:text-lg w-auto  mb-2 md:mb-3 font-semibold bg-gradient-to-l from-primary to-secondary text-white">
+        <h3 className="text-sm md:text-lg w-auto mb-2 md:mb-3 font-semibold bg-gradient-to-l from-primary to-secondary text-white">
           {name}
         </h3>
         <p className="flex text-gray-dark flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
@@ -77,6 +72,16 @@ const ProjectCard = ({
           ))}
         </p>
       </div>
+      {isModalOpen && (
+        <Modal
+          projects={projects}
+          currentIndex={currentIndex}
+          image={image}
+          previousProject={previousProject}
+          nextProject={nextProject}
+          closeModal={closeModal}
+        />
+      )}
     </div>
   );
 };
