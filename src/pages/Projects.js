07f16002b530af2fa0ffe509a9f79e440cf18e97
Tim Quattrochi <timq82@gmail.com>
2023-09-23 12:36:20 -0400

    event handlers for Modal, use Divider component

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,39 @@
 import ProjectCard from "../components/ProjectCard/ProjectCard.js";
 import { projects } from "../utils/projects";
-import Divider from "../components/Divider";
+import { Divider } from "../components/index.js";
+import { useState } from "react";
 
 function Projects() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const openModal = (e, index) => {
+    e.stopPropagation();
+    setCurrentIndex(index);
+    setIsModalOpen((prev) => !prev);
+  };
+
+  const closeModal = (e) => {
+    e.stopPropagation();
+    setIsModalOpen((prev) => !prev);
+    setCurrentIndex(0);
+  };
+
+  const nextProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projects.length);
+  };
+
+  const previousProject = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? projects.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div className="p-4" id="projects">
-      <div className=" flex flex-col h-full justify-center text-center text-1xl md:text-1xl lg:text-2xl font-heading font-medium static top-0 inset-x-0 p-2  bg-gradient-to-r from-indigo-500 transition-colors duration-200 ease-out transform origin-top-right"></div>
-      <div className="flex flex-row flex-wrap md:flex-nowrap gap-5">
+      <Divider text="Explore My Projects" forProjects={true} />
+
+      <div className="flex flex-row flex-wrap md:flex-nowrap gap-5 mt-8">
         {projects.map((project) => (
           <ProjectCard
             key={project.name}
@@ -14,6 +41,13 @@ function Projects() {
             link2={project.gitHubLink}
             tech={project.tech}
             {...project}
+            isModalOpen={isModalOpen}
+            openModal={openModal}
+            closeModal={closeModal}
+            nextProject={nextProject}
+            previousProject={previousProject}
+            currentIndex={currentIndex}
+            projects={projects}
           />
         ))}
       </div>
