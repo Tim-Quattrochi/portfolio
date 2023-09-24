80c00db8fe031a2517ca64ccd5122f3239ef9bc0
Tim Quattrochi <timq82@gmail.com>
2023-09-24 16:02:37 -0400

    added section tag for semantics

--- src/components/About.js
+++ src/components/About.js
@@ -3,24 +3,29 @@ import { FaChevronDown } from "react-icons/fa";
 
 function About() {
   return (
-    <>
+    <section>
       <p className="text-2xl md:text-4xl font-bold text-center text-white bg-gradient-to-r from-primary to-secondary py-2 px-4 md:py-4 md:px-6 border border-solid border-black hover:text-opacity-90 hover:bg-opacity-90 rounded-lg shadow-lg dark:bg-dark-background dark:text-dark-primary-text dark:from-dark-primary dark:to-dark-secondary">
         Welcome! 🤙
       </p>
-      <p className="text-xl text-white leading-relaxed md:leading-loose mt-4 my-2 mx-4 md:my-4 md:mx-6 dark:text-dark-primary-text">
-        Greetings, I am Tim, a seasoned full-stack developer. My
-        passion for both front-end and back-end work is unparalleled,
-        as I live and breathe coding from inception to completion.
-        During my free time, I recharge by taking leisurely walks
-        alongside my loyal Dalmatian companion and beautiful Wife.
-        Let's join forces and craft something truly unique together!
+      <p className="text-base text-white text-justify leading-relaxed md:leading-loose mt-4 my-2 mx-4 md:my-4 md:mx-6 dark:text-dark-primary-text">
+        Hello, I'm Tim, a seasoned full-stack developer deeply
+        passionate about both front-end and back-end development. My
+        coding journey is a continuous exploration, from project
+        inception to the final product. In my downtime, I enjoy
+        leisurely walks with my loyal Dalmatian companion and spending
+        quality time with my beloved wife. I'm enthusiastic about the
+        prospect of joining a dynamic team of developers and
+        contributing to the realization of our shared vision!
       </p>
       <ScrollIntoView selector="#tech">
         <div className="mx-auto p-10 mt-8">
-          <FaChevronDown className="animate-bounce mx-auto text-3xl text-white dark:text-dark-primary-text" />
+          <FaChevronDown
+            className="animate-bounce mx-auto text-3xl text-white dark:text-black"
+            size={34}
+          />
         </div>
       </ScrollIntoView>
-    </>
+    </section>
   );
 }
 
