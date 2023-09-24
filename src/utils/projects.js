14366229b649838cb4305f3ad914cd5039c4d234
Tim Quattrochi <timq82@gmail.com>
2023-09-23 22:58:03 -0400

    added images to projects array

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,4 +1,10 @@
 import { nt, tpd2, upfront, guestBook, timBlog } from "../assets";
+import {
+  rememPage2,
+  tpdTracker,
+  upfrontSearch,
+  upfrontSearch2,
+} from "../assets/webp";
 
 export const projects = [
   {
@@ -14,7 +20,7 @@ export const projects = [
   },
   {
     name: "Total Product Dev",
-    image: tpd2,
+    image: [tpd2, tpdTracker],
     description: `Total Product Development. I led the development and implementation of CRUD features for project proposals and user information. This resulted in a user-friendly platform with robust project management capabilities. I was responsible for constructing backend controllers, routes, and models for a MongoDB database, which ensured efficient data management and seamless integration with the front end.`,
     liveLink: "http://3.90.15.228/",
     gitHubLink: "https://github.com/Tim-Quattrochi/TPD",
@@ -22,7 +28,7 @@ export const projects = [
   },
   {
     name: "Remembrance Page",
-    image: guestBook,
+    image: [guestBook, rememPage2],
     description: `This web application serves as a memorial platform that allows users to register and access their accounts securely through JSON Web Tokens. The data storage and management system are facilitated by MongoDB, with Mongoose schema ensuring the accuracy and safety of all stored data. The user interface is designed using Bootstrap, CSS, and responsive web design principles, providing an engaging and easy-to-use experience.`,
     liveLink: "https://www.jerrykrikava.com",
     gitHubLink: "https://github.com/Tim-Quattrochi/Remembrance-Page",
@@ -31,7 +37,7 @@ export const projects = [
 
   {
     name: "Local Insights",
-    image: upfront,
+    image: [upfront, upfrontSearch2, upfrontSearch],
     description: `I developed a strong business review website using the MERN stack and adhered to best practices for clean code and architecture. I utilized Tailwind CSS to create a professional and responsive user interface. Additionally, I incorporated a dynamic feature that allows users to submit businesses and a feature-rich review system that allows for image uploads. To improve user experience, I implemented pagination and designed an easy-to-use user profile page for reviewing.
 `,
     liveLink: "http://54.90.137.205/",
