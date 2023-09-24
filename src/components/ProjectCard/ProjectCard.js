26f965f46056e07ef606a7ff22a3924cc95bb86e
Tim Quattrochi <timq82@gmail.com>
2023-09-24 15:22:30 -0400

    styling adjustments, moved name, description to modal

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -4,7 +4,6 @@ import Modal from "../Modal/MyModal";
 const ProjectCard = ({
   name,
   image,
-  description,
   tech,
   link,
   link2,
@@ -21,67 +20,71 @@ const ProjectCard = ({
     (proj) => proj.name === name
   );
 
+  //this function prevents the modal from opening when the user clicks on the live demo and
+  //github links.
+  const linkHandler = (e) => {
+    e.stopPropagation();
+  };
+
   return (
     <div
-      className="relative rounded-lg overflow-hidden h-full group"
+      className="relative rounded-lg overflow-hidden h-full "
       onClick={() => openModal(clickedIndex)}
     >
       <img
         src={image instanceof Array ? image[0] : image}
         alt="work"
-        className="w-full h-48 md:h-64 object-cover transition-transform transform  group-hover:scale-105"
+        className="w-full h-48 md:h-64 object-cover transition-transform transform  group-hover:scale-105 cursor-pointer"
       />
-      <div className="absolute inset-0 bg-gradient-to-t from-primary to-secondary text-white opacity-0 transition-opacity duration-300  flex flex-col justify-center items-center p-5  cursor-pointer">
-        <h3 className="text-md md:text-xl mb-1 md:mb-3 font-semibold text-center">
-          {name}
-        </h3>
-        <p className="text-xs mb-2 text-center font-bold text-white leading-none bg-opacity-90 bg-dark-accent-1 rounded-md">
-          {description}
-        </p>
 
-        <div className="flex justify-center space-x-4 mt-2">
-          {link && (
-            <a
-              href={link}
-              target="_blank"
-              rel="noreferrer"
-              className="text-text bg-accent hover:text-blue transition-colors duration-300 px-3 py-2 rounded-md"
-            >
-              Live Demo
-            </a>
-          )}
-          {link2 && (
-            <a
-              href={link2}
-              target="_blank"
-              rel="noreferrer"
-              className="text-[#000080] bg-primary hover:text-secondary transition-colors duration-300 px-3 py-2 rounded-md"
-            >
-              GitHub
-            </a>
-          )}
-        </div>
-      </div>
-      <div className="bg-primary p-5 text-text-gray-dark">
-        <h3 className="text-sm md:text-lg w-auto mb-2 md:mb-3 font-semibold bg-gradient-to-l from-primary to-secondary text-white">
+      <div className="bg-primary p-5 text-text-gray-dark  dark:bg-dark-background">
+        <h3 className="text-sm md:text-lg w-auto mb-2 md:mb-3 font-semibold bg-gradient-to-l from-primary to-secondary text-white text-center p-1 dark:text-dark-primary-text">
           {name}
         </h3>
-        <p className="flex text-gray-dark flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
+        <p className="flex text-gray-dark w-auto flex-wrap gap-2 flex-row items-center justify-center text-xs md:text-sm">
           {tech.map((item, i) => (
             <span
               key={i}
-              className="inline-block px-2 py-1 bg-gray-light text-gray-dark rounded-md"
+              className="px-1 py-1 z-10 lg:px-2 lg:py-2 hover:bg-white bg-gray-light text-gray-dark rounded"
             >
               {item}
             </span>
           ))}
         </p>
+        <div>
+          {/*start of links */}
+          <div className="relative z-10 flex justify-center space-x-3 mt-5">
+            {link && (
+              <a
+                href={link}
+                target="_blank"
+                rel="noreferrer"
+                onClick={linkHandler}
+                className="text-text font-bold bg-accent hover:text-blue-200 transition-colors duration-300 px-3 py-2 rounded-md"
+              >
+                Live Demo
+              </a>
+            )}
+            {link2 && (
+              <a
+                href={link2}
+                target="_blank"
+                rel="noreferrer"
+                onClick={linkHandler}
+                className="text-text font-bold bg-white hover:text-secondary transition-colors duration-300 px-3 py-2 rounded-md"
+              >
+                GitHub
+              </a>
+            )}
+          </div>
+          {/*end of links */}
+        </div>
       </div>
+
       {isModalOpen && (
         <Modal
           projects={projects}
           currentIndex={currentIndex}
-          image={image}
           previousProject={previousProject}
           nextProject={nextProject}
           closeModal={closeModal}
