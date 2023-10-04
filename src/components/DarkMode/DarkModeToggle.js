8ad8524c001a8612f779b06fa98ca660f688ea71
Tim Quattrochi <timq82@gmail.com>
2023-10-04 09:15:17 -0400

    got rid of the toggle for darkmode so it's only the svg that switches

--- src/components/DarkMode/DarkModeToggle.js
+++ src/components/DarkMode/DarkModeToggle.js
@@ -7,47 +7,27 @@ const DarkModeToggle = () => {
   const { darkmode, toggleDarkMode } = useDarkMode();
 
   return (
-    <div className="flex items-center justify-center">
-      <label
-        htmlFor="toggle"
-        className="cursor-pointer flex items-center transition-colors duration-300 ease-in-out"
-      >
-        <div className="relative w-12 h-6 bg-gray-300 rounded-full dark:bg-gray-800 dark:text-white">
-          <input
-            type="checkbox"
-            id="toggle"
-            className="sr-only"
-            checked={darkmode}
-            onChange={toggleDarkMode}
-          />
-          <div
-            className={`toggle-dot absolute w-6 h-6 bg-white rounded-full shadow-md transform duration-300 ${
-              darkmode ? "translate-x-6" : ""
-            }`}
-          ></div>
-        </div>
-        <span
-          className={`text-gray-600 dark:text-gray-300 mx-2 ${
-            darkmode ? "order-1" : "order-2"
-          }`}
-        >
-          {darkmode ? (
-            <img
-              style={{ borderRadius: "50px" }}
-              width={40}
-              src={moon}
-              alt="moon"
-            />
-          ) : (
-            <img
-              style={{ borderRadius: "50px" }}
-              width={40}
-              src={sun}
-              alt="sun"
-            />
-          )}
-        </span>
-      </label>
+    <div className="flex items-center justify-center mr-6 md:ml-4">
+      {darkmode ? (
+        <img
+          className="rounded-full cursor-pointer"
+          width={40}
+          src={moon}
+          alt="moon"
+          checked={darkmode}
+          onClick={toggleDarkMode}
+        />
+      ) : (
+        <img
+          // style={{ borderRadius: "50px", cursor: "pointer" }}
+          className="rounded-full cursor-pointer"
+          width={40}
+          src={sun}
+          alt="sun"
+          checked={darkmode}
+          onClick={toggleDarkMode}
+        />
+      )}
     </div>
   );
 };
